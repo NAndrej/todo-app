9740c32fee020b34daaaa8c8d1e8c4b7ad140c80
Andrej Naumovski <andrej@embedsocial.com>
2021-07-16 12:57:57 +0200

    a working solution

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,20 +4,58 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
-class TodoController extends AbstractController{
+use App\Entity\Task;
 
-    private $itemList = array(
-        "Daily meeting", 
-        "Videos"
-    );
+class TodoController extends AbstractController
+{
 
     /**
-     * @Route("/hello")
+     * @Route("/", name="index", methods={"GET"}, name="home")
      */
-    public function hello(){
+    public function index()
+    {
         return $this->render("index.html.twig", [
-            'items' => $this->itemList,
+            'items' => $this->fetchTasks(),
         ]);
+    }    
+
+    /**
+     * @Route("/insert", methods={"POST"}, name="insert_task")
+     */
+    public function insertTask()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task($_POST["task"]);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @Route("/delete/id?={id}", methods={"GET"}, name="delete_task")
+     */
+    public function deleteTask(int $id){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $currentTask = $entityManager
+                    ->getRepository(Task::class)
+                    ->findOneBy(array(
+                        "id" => $id
+                    ));
+
+        $entityManager->remove($currentTask);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('home');
+    }
+
+    public function fetchTasks(){
+        $entityManager = $this->getDoctrine()
+                            ->getRepository(Task::class);
+        return $entityManager->findAll();
     }
 }
---
+++ src/Entity/Task.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tasks")
+ */
+class Task
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $text;
+    
+    public function __construct(String $text)
+    {
+        $this->text = $text;
+    }
+
+    public function getText(): String
+    {
+        return $this->text;
+    }
+
+    public function setText(String $text)
+    {
+        $this->text = $text;
+    }
+    
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
